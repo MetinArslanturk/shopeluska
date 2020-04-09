2dd7f7883fdedbd50e408547d9978de2c48e0e5c
Metin Arslanturk <metin@Metins-MacBook-Pro.local>
2020-04-09 20:38:20 +0300

    clean code

--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import {LoginPage} from '../../components/LoginPage';
+import { LoginPage } from '../../components/LoginPage';
 import { shallow } from 'enzyme';
 
 
 
 test('should render login page when non-authenticated', () => {
-    const startLogin = jest.fn();
-
-    const wrapper = shallow(<LoginPage isAuthenticated={false} startLogin={startLogin} />)
+    const wrapper = shallow(<LoginPage isAuthenticated={false} />)
     expect(wrapper).toMatchSnapshot();
+});
 
-    
-    wrapper.find('#loginform').prop('onFinish')({username: 'aa', password:'123'});
+
+test('should trigger startLogin when form trigger onFinish', () => {
+    const startLogin = jest.fn();
+    const wrapper = shallow(<LoginPage isAuthenticated={false} startLogin={startLogin} />)
+    wrapper.find('#loginform').prop('onFinish')({ username: 'aa', password: '123' });
     expect(startLogin).toHaveBeenCalled();
 });
 
 
 test('should redirect when authenticated', () => {
-    const prop = {
-        isAuthenticated: true
-    }
-    const wrapper = shallow(<LoginPage {...prop} />)
+    const wrapper = shallow(<LoginPage isAuthenticated={true} />)
     expect(wrapper).toMatchSnapshot();
 });
