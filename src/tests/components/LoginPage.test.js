b45e9458dffe5581921f698ad2cd99e6c52c50f5
Metin Arslanturk <metin@Metins-MacBook-Pro.local>
2020-04-10 11:22:41 +0300

    login test wrap up

--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { LoginPage } from '../../components/LoginPage';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 
 
@@ -15,6 +15,17 @@ test('should trigger startLogin when form trigger onFinish', () => {
     const wrapper = shallow(<LoginPage isAuthenticated={false} startLogin={startLogin} />)
     wrapper.find('#loginform').prop('onFinish')({ username: 'aa', password: '123' });
     expect(startLogin).toHaveBeenCalled();
+});
+
+test('should trigger useMount and hideSidebar when mount also showSidebar on unmount', () => {
+    const startLogin = jest.fn();
+    const hideSidebar = jest.fn();
+    const showSidebar = jest.fn();
+    const wrapper = mount(<LoginPage isAuthenticated={false} startLogin={startLogin} showSidebar={showSidebar} hideSidebar={hideSidebar} />)
+    expect(hideSidebar).toHaveBeenCalled();
+
+    wrapper.unmount();
+    expect(showSidebar).toHaveBeenCalled();
 });
 
 
