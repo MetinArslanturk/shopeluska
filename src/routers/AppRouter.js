9af5ce7f5411b2391cba263144bd9eed3e75e2ef
Metin Arslanturk <metin@jotform.com>
2020-05-13 13:57:53 +0300

    eslint

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Redirect } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import PublicRoute from './PublicRoute';
 import PrivateRoute from './PrivateRoute';
 import MainPage from '../components/pages/MainPage';
@@ -8,7 +9,6 @@ import LoginPage from '../components/pages/LoginPage';
 import ProductPage from '../components/pages/ProductPage';
 import ShoppingCart from '../components/pages/ShoppingCart';
 import CompleteOrder from '../components/pages/CompleteOrder';
-import { Helmet } from 'react-helmet';
 
 const AppRouter = () => {
     return (
@@ -16,19 +16,34 @@ const AppRouter = () => {
             <Helmet>
                 <title>Shopeluska</title>
             </Helmet>
-        <Switch>
-            <PublicRoute path="/" component={MainPage} exact={true}/>
-            <PublicRoute path="/login" component={LoginPage} exact={true}/>
-            <PublicRoute path="/product/:id" component={ProductPage} exact={true}/>
-            <PublicRoute path="/shopping-cart" component={ShoppingCart} exact={true}/>
-            <PrivateRoute path="/my-account" component={MyAccountPage} exact={true} />
-            <PrivateRoute path="/payment-step" component={CompleteOrder} redirectUrl={'payment-step'} exact={true} />
-            <Redirect to="/404" />
-        </Switch>
+            <Switch>
+                <PublicRoute path="/" component={MainPage} exact />
+                <PublicRoute path="/login" component={LoginPage} exact />
+                <PublicRoute
+                    path="/product/:id"
+                    component={ProductPage}
+                    exact
+                />
+                <PublicRoute
+                    path="/shopping-cart"
+                    component={ShoppingCart}
+                    exact
+                />
+                <PrivateRoute
+                    path="/my-account"
+                    component={MyAccountPage}
+                    exact
+                />
+                <PrivateRoute
+                    path="/payment-step"
+                    component={CompleteOrder}
+                    redirectUrl="payment-step"
+                    exact
+                />
+                <Redirect to="/404" />
+            </Switch>
         </>
     );
-}
-
-
+};
 
 export default AppRouter;
